dacd913fd5e1591ff2b1559d29d401d5cc288af6
JessicaWildermuth <jwildermuth536@gmail.com>
2020-08-27 11:22:46 -0700

    Rooks Solutions Found

--- src/solvers.js
+++ src/solvers.js
@@ -16,15 +16,67 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  //chess piece count set at n to begin with
+  let board = new Board({n});
+  let solution = board.rows();
+  let chessPieces = n;
+  // base case - when there are no chess pieces left
+  //average
+  // loop through the board to access the rows
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  for (var row = 0; row < solution.length; row++) {
+    //loop through the current row (get cols)
+    for (var col = 0; col < solution[row].length; col++) {
+      //check if the current row[col] is empty (aka 0);
+      board.togglePiece(row, col);
+      //decrement piece count
+      chessPieces--;
+      if (board.hasAnyRooksConflicts()) {
+        //check if there are any rook conflicts
+        //if yes, remove piece and increment--;
+        board.togglePiece(row, col);
+        chessPieces++;
+      } else {
+        break;
+      }
+    }
+  }
+
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(board));
+
+  return board.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+  let callcount = 0;
+
+  //   [0,0,1,0,0]    // [0,1,0,0,0]   // [0,0,1,0,0]    // [0,0,0,1,0] //
+  //   [0,1,0,0,0]    // [0,0,1,0,0]   // [0,0,0,1,0]
+  //-> [1,0,0,0,0]    // [1,0,0,0,0]
+  //-> [0,0,0,0,0]    // [1,0,0,0,0]
+
+  let backtracking = function(board, count, rowIndex) {
+    if (count === 0) {
+      solutionCount++;
+    } else {
+      for (let colIndex = 0; colIndex < board.rows().length; colIndex++) {
+        board.togglePiece(rowIndex, colIndex);
+        if (board.hasAnyRooksConflicts()) {
+          board.togglePiece(rowIndex, colIndex);
+        } else {
+          let newCount = count - 1;
+          let newrowIndex = rowIndex + 1;
+          backtracking(board, newCount, newrowIndex);
+          board.togglePiece(rowIndex, colIndex);
+        }
+      }
+    }
+  };
+
+  backtracking(new Board({n}), n, 0);
+
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
